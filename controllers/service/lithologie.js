012529df2e439fa4ac719d51f06596d3cdf78c52
anjaNaly <randriamanalinaanja@gmail.com>
2024-02-05 10:25:04 +0300

    obtention des données nécessaires au krigeage
    les données : x y des coordonnées des points des traces d'une substances avec les frequences et les nombres de traces

--- controllers/service/lithologie.js
+++ controllers/service/lithologie.js
@@ -1,30 +1,11 @@
 //lithologie.js
 //avoir les coordonnées des couches présentants des traces de la substance recherché
+const krigeage = require('../fonction/kriging.js');
 const pool = require('../../database.js'); // Importez la configuration de la base de données
 
-const getCoucheSubs = (substance) => {
-    return 'SELECT * FROM get_couches_subs(\'' + substance + '\')';
-}
-
-//fonction qui donne la liste des codes des couches qui intersectent les traces de substance et leurs frequences
-const getGroupLithoIntersect = async(coucheSubs) => {
+const getNbCoucheLitho = async(nomSubstances)=>{
     return new Promise((resolve, reject) => {
-        const request = 'SELECT lithology.code, lithologie, count(*) as nombre FROM lithology WHERE ST_Intersects(ST_SetSRID(geom, 4326), ST_SetSRID(\'' + coucheSubs + '\'::geometry,4326)) group by lithology.code , lithologie ;';
-        pool.query(request, (error, resultat) => {
-            if (error) {
-                console.error(error);
-                reject('Erreur de base de données');
-            } else {
-                const code = resultat.rows;
-                resolve(code);
-            }
-        });
-    });
-}
-// avoir le nombre de couche qui intersect les traces de substances
-const getNombreCoucheIntersect = async (coucheSubs) => {
-    return new Promise((resolve, reject) => {
-        const request = 'select count(*) FROM lithology l WHERE ST_Intersects(ST_SetSRID(geom, 4326), ST_SetSRID(\'' + coucheSubs + '\'::geometry,4326)) ';
+        const request = "select count(*) from (select l.code from lithology l , get_intersections('Nickel') as tracesSubs where ST_Intersects(ST_SetSRID(l.geom, 4326), ST_SetSRID(tracesSubs.traces,4326)) group by  l.code, l.lithologie, l.geom ) as requ;";
         pool.query(request, (error, resultat) => {
             if (error) {
                 console.error(error);
@@ -36,14 +17,29 @@ const getNombreCoucheIntersect = async (coucheSubs) => {
         });
     });
 };
-//avoir les codes des couches qui intersectents les traces de substances, les coordonnées de ces traces et les frequences de ces codes
-//les pourcentages ne sont pas encore vraiment là , juste la liste de fréquence , on aura besoin du nombre de ligne du résulat de cette requete pour pouvoir calculer ce pourcentage
-const getPourcentage = async (listeCouche ,coucheSubs )=>{
-    let requete = 'SELECT lithology.code, ST_Intersection(ST_SetSRID(geom, 4326), ST_SetSRID(\'' + coucheSubs + '\'::geometry,4326))AS intesection,case ';
-    listeCouche.forEach(code => {
-        requete = requete+ 'when code=\''+code["code"]+'\' then '+code["nombre"]+' ';
+const getFrequence = async(nomSubstances)=>{
+    return new Promise((resolve, reject) => {
+        const request = "select code ,count(*) as nombre from (select l.code from lithology l , get_intersections('"+nomSubstances+"') as tracesSubs where ST_Intersects(ST_SetSRID(l.geom, 4326), ST_SetSRID(tracesSubs.traces,4326)) group by  l.code, l.lithologie, l.geom ) as requ group by code;";
+        pool.query(request, (error, resultat) => {
+            if (error) {
+                console.error(error);
+                reject('Erreur de base de données');
+            } else {
+                const codefrequence = resultat.rows;
+                resolve(codefrequence);
+            }
+        });
     });
-    requete =requete+'else 0 end as pourcentage FROM lithology WHERE ST_Intersects(ST_SetSRID(geom, 4326), ST_SetSRID(\'' + coucheSubs + '\'::geometry,4326))';
+};
+
+///fonction pour avoir les code x y et pourcentage 
+const getCoordonnées = async (codefrequence , nomSubs)=>{
+    let requete = 'select code2,ST_X(points::geometry) as x,ST_Y(points::geometry) as y,case ';
+    codefrequence.forEach(code => {
+        requete = requete+ ' when code2 =\''+code["code"]+'\' then '+code["nombre"]+' ';
+    });
+    requete =requete+'else 0 end as pourcentage ';
+    requete = requete +" from get_toutpoints('"+nomSubs+"') as get_tout";
     return new Promise((resolve, reject) => {
         pool.query(requete, (error, resultat) => {
             if (error) {
@@ -55,10 +51,12 @@ const getPourcentage = async (listeCouche ,coucheSubs )=>{
             }
         });
     });
-}
+};
+// const getResultKrigeage = async (codefrequence , nomSubs)=>{
+
+// };
 module.exports = {
-    getCoucheSubs,
-    getGroupLithoIntersect,
-    getNombreCoucheIntersect,
-    getPourcentage,
+    getCoordonnées,
+    getFrequence,
+    getNbCoucheLitho
 };
--- controllers/testController.js
+++ controllers/testController.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database.js'); // Importez la configuration de la base de données
-const { getCoucheSubs, getGroupLithoIntersect, getNombreCoucheIntersect, getPourcentage } = require('./service/lithologie');
+const {getCoordonnées,getFrequence,getNbCoucheLitho } = require('./service/lithologie');
 
-router.post('/getPourcentage', async (req, res) => {
+router.post('/getest', async (req, res) => {
     const substance = req.body.substance;
     try {
-        pool.query(getCoucheSubs(substance), async (error, results) => {
-            if (error) {
-                console.error(error);
-                res.status(500).send('Erreur de base de données');
-            } else {
-                const codeCoucheIntersect = await getGroupLithoIntersect(results.rows[0].get_couches_subs);
-                const requetepourcentage = await getPourcentage(codeCoucheIntersect,results.rows[0].get_couches_subs);
-                const responseData = {
-                    nombre: requetepourcentage.length,
-                    requetepourcentage: requetepourcentage,
-                }
-                res.json(responseData);
-            }
-        });
+        const nbCoucheLitho = await getNbCoucheLitho(substance);
+        const codefrequence = await getFrequence(substance);
+        const getCoordXY = await getCoordonnées(codefrequence,substance);
+        const responseData = {
+            nombre :nbCoucheLitho,
+            requetepourcentage: getCoordXY,
+        }
+        res.json(responseData);
+        
     } catch (error) {
         console.error(error);
     }
-});
-
+})
 
 module.exports = router;
